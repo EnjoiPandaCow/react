99bd62cb8035d8ede6de401a3b478b8ed38d6559
EnjoiPandaCow <cjosullivan@hotmail.co.uk>
2020-12-03 11:41:43 +0000

    Added a click listener on the button and method to output a console log when the button is clicked

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -12,10 +12,14 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    console.log('Was clicked');
+  }
+
   render() {
     return (
       <div className="App">
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My hobbies: Cooking</Person>
