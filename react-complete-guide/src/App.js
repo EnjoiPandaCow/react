73c7d68db53d7928730623f9e7ab49c4af9b74d0
EnjoiPandaCow <cjosullivan@hotmail.co.uk>
2020-12-03 11:24:03 +0000

    Added the state property to the App.js component and a button to manipulate state later

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'CJ', age: 26},
+      { name: 'Dvaid', age: 40},
+      { name: 'Crona', age: 46},
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hi, I am a React app.</h1>
-        <p>This is really working.</p>
-        <Person name="CJ" age="26" />
-        <Person name="David" age="40" />
-        <Person name="Crona" age="46">My hobbies: Cooking</Person>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My hobbies: Cooking</Person>
       </div>
     );
   }
