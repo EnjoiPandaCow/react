ed5746c3080249bdb415cf1b2085006ef4d0799a
EnjoiPandaCow <cjosullivan@hotmail.co.uk>
2021-01-04 11:05:00 +0000

    - Passed a reference to a method as a property of a compoent. - This allows for you to call a method and change state in a component that does not have direct access to state.
    - Used .bind() and created an arrow function to pass a value to switchNameHandler

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -7,17 +7,17 @@ class App extends Component {
   state = {
     persons: [
       { name: 'CJ', age: 26},
-      { name: 'Dvaid', age: 40},
+      { name: 'David', age: 40},
       { name: 'Crona', age: 46},
     ],
     otherState: 'another value'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     this.setState( {
       persons: [
-        { name: 'Christopher John', age: 26},
-        { name: 'Dvaid', age: 40},
+        { name: newName, age: 26},
+        { name: 'David', age: 40},
         { name: 'Crona', age: 46},
       ]
     } )
@@ -26,10 +26,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My hobbies: Cooking</Person>
+        <button onClick={() =>  this.switchNameHandler('Christopher John')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} 
+          click={this.switchNameHandler.bind(this, 'Ceejee')}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age} />
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}>My hobbies: Cooking</Person>
       </div>
     );
   }
