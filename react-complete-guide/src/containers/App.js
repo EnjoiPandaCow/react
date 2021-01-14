19fa03fab109dc3bb7311b3933ac4104d77f467c
EnjoiPandaCow <cjosullivan@hotmail.co.uk>
2021-01-14 10:46:35 +0000

    Added update lifecycle hooks to show how they work when state is updated.

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -31,6 +31,15 @@ class App extends Component {
     console.log('[App.js] componentDidMount');
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate () {
+    console.log('[App.js] componentDidUpdate');
+  }
+
   nameChangedHandler = (event, id) => {
     // Finding the person in the array, in state that we want to change.
     const personIndex = this.state.persons.findIndex(p => {
