4d5e6bfa4a48fa5a01c2790ab62ece9416d70136
EnjoiPandaCow <cjosullivan@hotmail.co.uk>
2021-01-19 08:58:52 +0000

    Returend adjacent components using an aux higher order component.

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 import classes from "./Person.css";
+import Aux from '../../../hoc/Aux'
 
 class Person extends Component {
   render() {
     console.log("[Person.js] rendering ...");
     return (
-      <div className={classes.Person}>
-        {/* This needs to be used to access props of a class based component. */}
-        <p onClick={this.props.click}>
+    <Aux>
+        {/* This needs to be used to access props of a class based component. 
+        Converted this to be an array of elements to return adjacent elements. */}
+        <p key="i1" onClick={this.props.click}>
           Im {this.props.name} and I am {this.props.age} years old!
         </p>
-        <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </div>
+        <p key="i2">{this.props.children}</p>
+        <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />
+    </Aux>
     );
   }
 }
---
+++ react-complete-guide/src/hoc/Aux.js
@@ -0,0 +1,4 @@
+// Children is a special property that simply outputs what ever gets entered between the opening and closing tag of this component. 
+const aux = props => props.children
+
+export default aux; 
