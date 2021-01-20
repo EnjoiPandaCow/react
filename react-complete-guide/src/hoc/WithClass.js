b155a6a112e1d76686edafd21e56cf317f762343
EnjoiPandaCow <cjosullivan@hotmail.co.uk>
2021-01-20 09:05:47 +0000

    Showed how to pass unknown props to a child of a HOC.

--- react-complete-guide/src/hoc/WithClass.js
+++ react-complete-guide/src/hoc/WithClass.js
@@ -4,10 +4,13 @@ import React from 'react';
 // We aren't returning JSX we are actually returning another function definition and that is the function definition of a compoent function. 
 const withClass = (WrappedCompoent, className) => {
     // This is a functional component. 
+    // This is the props of our wrapped component 
     return props => (
         <div className={className}>
             {/* This is a component that we are reciving */}
-            <WrappedCompoent />
+            {/* The props we are getting here are a JS object, the spread operator then pulls out all of the properties that ar inside of this props object 
+            and distributes them as new key value pairs. */}
+            <WrappedCompoent {...props}/>
         </div>
     );
 }
