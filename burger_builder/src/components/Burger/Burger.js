cadd2ab57fd357d2be7eb5b36df77f04f62107b8
EnjoiPandaCow <cjosullivan@hotmail.co.uk>
2021-01-28 09:54:02 +0000

    Altered the burger compoent to putput ingredients dynamically.

--- burger_builder/src/components/Burger/Burger.js
+++ burger_builder/src/components/Burger/Burger.js
@@ -4,11 +4,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css'
 
 const burger = (props) => {
+
+    // Breaks down the ingredients objects keys in to an array. 
+    const transformendIngredients = Object.keys(props.ingredients)
+        // Executes a function on each element in the input array. 
+        .map(igKey => {
+            // Transfrom the string value into as many elements as we have ingredients for a given ingredient
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformendIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
