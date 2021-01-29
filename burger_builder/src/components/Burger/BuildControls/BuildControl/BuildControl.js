35a705510b88ad8eb59ca9b70c32f4cbe58bb334
EnjoiPandaCow <cjosullivan@hotmail.co.uk>
2021-01-29 08:17:11 +0000

    Added the build controls and build control component.

---
+++ burger_builder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from './BuildControl.css';
+
+const buildControl = (props) => (
+    <div className={classes.BuildControl}>
+        <div className={classes.Label}>{props.label}</div>
+        <button className={classes.Add}>Add</button>
+        <button className={classes.Remove}>Remove</button>
+    </div>
+);
+export default buildControl;
