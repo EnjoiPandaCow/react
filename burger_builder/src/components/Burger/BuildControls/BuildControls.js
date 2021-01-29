24fc6bbd46dd03af2d8bc923ca5a128f135e9a58
EnjoiPandaCow <cjosullivan@hotmail.co.uk>
2021-01-29 08:29:54 +0000

    Added the build controls to the burger builder component.

--- burger_builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger_builder/src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const buildControls = (props) => {
-    <div>
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
 
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Meat', type: 'cheese'},
+    {label: 'Meat', type: 'meat'}
+]
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        {/* Loop through all of the controls to output a BuildComponent for each. */}
+        {controls.map(ctrl => (
+            <BuildControl key={ctrl.label} label={ctrl.label}/>
+        ))}
     </div>
-};
+);
 
 export default buildControls;
