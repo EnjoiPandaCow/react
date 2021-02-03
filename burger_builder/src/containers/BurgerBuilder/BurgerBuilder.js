a1deaae88111c4ee1490ade5898b09811b44eb04
EnjoiPandaCow <cjosullivan@hotmail.co.uk>
2021-02-03 08:51:46 +0000

    Added a backdrop to the modal, when clicked the modal disapears.

--- burger_builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger_builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,6 +93,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: true});
     }
 
+    purchasableCancelHandler = () => {
+        this.setState({purchasing: false});
+    }
+
     render () {
 
         const disabledInfo = {
@@ -106,7 +110,7 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalClosed={this.purchasableCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
